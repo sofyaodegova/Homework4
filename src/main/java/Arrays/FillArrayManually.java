0307f8cb158c14cf1923c8b2e61c2e9e42aaaceb
Sofya Odegova <sophiaodegova@mail.ru>
2022-11-05 21:05:53 +0400

    1st task is done

---
+++ src/main/java/Arrays/FillArrayManually.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+public class FillArrayManually {
+    public static void main(String[] args) {
+        int[] array1 = new int[10];
+        array1[0] = 0;
+        array1[1] = 1;
+        array1[2] = 2;
+        array1[3] = 3;
+        array1[4] = 4;
+        array1[5] = 5;
+        array1[6] = 6;
+        array1[7] = 7;
+        array1[8] = 8;
+        array1[9] = 9;
+
+        System.out.println(array1[5]); // - пример вывода элемента массива в консоль
+
+        int[] array2 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        System.out.println(array2[3]); // - пример вывода элемента массива в консоль
+    }
+}
