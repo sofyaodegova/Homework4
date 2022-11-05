a15f05ec8cc0610b1698f93aa1f19a79d4345a38
Sofya Odegova <sophiaodegova@mail.ru>
2022-11-05 21:16:36 +0400

    3rd task is done

---
+++ src/main/java/Arrays/ArrayWithNumber.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+public class ArrayWithNumber {
+    public static void main(String[] args) {
+        int[] array = {1, 4, 7, 45, 123, 5, 896, 1064, 0, 98};
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println((i) + " - " + array[i]);
+        }
+    }
+}
---
+++ src/main/java/Arrays/PrintArray.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class PrintArray {
+    public static void main(String[] args) {
+
+        int[] array1 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        System.out.println(array1[0]);
+        System.out.println(array1[1]);
+        System.out.println(array1[2]);
+        System.out.println(array1[3]);
+        System.out.println(array1[4]);
+        System.out.println(array1[5]);
+        System.out.println(array1[6]);
+        System.out.println(array1[7]);
+        System.out.println(array1[8]);
+        System.out.println(array1[9]);
+    }
+}
