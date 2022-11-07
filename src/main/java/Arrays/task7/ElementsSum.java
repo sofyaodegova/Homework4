cd6428c7479758d030c1a18892a77665be80b8fb
Sofya Odegova <sophiaodegova@mail.ru>
2022-11-07 19:39:28 +0400

    7 task is done

---
+++ src/main/java/Arrays/task7/ElementsSum.java
@@ -0,0 +1,26 @@
+package Arrays.task7;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ElementsSum {
+
+    public static void main(String[] args) {
+        System.out.println("Сумма равна: " + randomArray(10, 0, 0));
+        ;
+    }
+
+    public static int randomArray(int n, int i, int sum) {
+
+        int[] array = new int[n];
+        int number = ThreadLocalRandom.current().nextInt(101);
+
+        if (i >= n) {
+            return sum;
+        }
+
+        sum += number;
+        array[i] = number;
+        System.out.println(array[i]);
+        return randomArray(n, ++i, sum);
+    }
+}
