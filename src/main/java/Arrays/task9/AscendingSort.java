63701918574f752156d65e801f05335ef98bf9a4
Sofya Odegova <sophiaodegova@mail.ru>
2022-11-08 15:32:31 +0400

    9 task is done

---
+++ src/main/java/Arrays/task9/AscendingSort.java
@@ -0,0 +1,25 @@
+package Arrays.task9;
+
+import java.util.Arrays;
+
+public class AscendingSort {
+    public static void main(String[] args) {
+        System.out.println("Is array sorted? " + isSorted());
+    }
+
+    public static boolean isSorted() {
+
+        int[] array = {5, 56, 8, 23, 78, 63, 6, 42, 2, 13};
+
+        Arrays.sort(array);
+        /*for (int values : array) {
+            System.out.print(values + " "); - можно включить, если нужно вывести уже отсортированный массив
+        }*/
+
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1])
+                return false;
+        }
+        return true;
+    }
+}
