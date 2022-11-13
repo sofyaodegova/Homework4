7e62722758e47842934bb6ba9df0b549b188c514
Sofya Odegova <sophiaodegova@mail.ru>
2022-11-13 16:18:12 +0400

    12 task is done

--- src/main/java/Arrays/task12/WorkWeek.java
+++ src/main/java/Arrays/task12/WorkWeek.java
@@ -1,25 +1,22 @@
 package Arrays.task12;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class WorkWeek {
     public static void main(String[] args) {
+
         int[] workHours = {8, 6, 8, 9, 6, 0, 0, 6, 10, 8, 8, 6, 0, 0};
+        int count = workHours.length / 7;
+        int[] hoursOfWeek = new int[count];
+        int sum = 0;
 
-        int week1 = 0;
-        int week2 = 0;
-
-        for (int i = 0; i < workHours.length - 7; i++) {
-            week1 = week1 + workHours[i];
+        for (int n = 0; n < count; n++) {
+            for (int i = 0; i < 7; i++) {
+                sum += workHours[i] - hoursOfWeek[n];
+            }
+            hoursOfWeek[n] = sum;
         }
-        for (int i = 7; i < workHours.length; i++) {
-            week2 = week2 + workHours[i];
-        }
-
-        int[] weekHours = new int[2];
-        weekHours[0] = week1;
-        weekHours[1] = week2;
-        System.out.println(Arrays.toString(weekHours));
+        System.out.println("Работник работал за " + count + " недели " + Arrays.toString(hoursOfWeek) + " часов " +
+                "соответственно.");
     }
 }
