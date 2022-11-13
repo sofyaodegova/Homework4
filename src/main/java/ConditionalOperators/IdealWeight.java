c1bf31a81acb299d72ad894a1aae6af6e1a4e3e2
Sofya Odegova <sophiaodegova@mail.ru>
2022-11-13 17:52:48 +0400

    conditional operators. 1st task is done.

---
+++ src/main/java/ConditionalOperators/IdealWeight.java
@@ -0,0 +1,24 @@
+package ConditionalOperators;
+
+public class IdealWeight {
+    public static void main(String[] args) {
+
+        int height = 180;
+
+
+        double idealWeight = (height - 100) - 0.1;
+        System.out.println("Идеальный вес: " + idealWeight + " +- 5 кг.");
+        double min = idealWeight - 5.0;
+        double max = idealWeight + 5.0;
+
+        double weight = 82.55;
+
+        if (weight < min) {
+            System.out.println("Ваш вес не идеален");
+        } else if (weight > max) {
+            System.out.println("Ваш вес не идеален");
+        } else {
+            System.out.println("Ваш вес идеален");
+        }
+    }
+}
