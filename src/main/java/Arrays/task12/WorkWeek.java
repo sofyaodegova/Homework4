6eda8991c2e3b56629200bc8c37d45a021e2f068
Sofya Odegova <sophiaodegova@mail.ru>
2022-11-14 19:46:11 +0400

    conditional operators 1

--- src/main/java/Arrays/task12/WorkWeek.java
+++ src/main/java/Arrays/task12/WorkWeek.java
@@ -5,18 +5,29 @@ import java.util.Arrays;
 public class WorkWeek {
     public static void main(String[] args) {
 
-        int[] workHours = {8, 6, 8, 9, 6, 0, 0, 6, 10, 8, 8, 6, 0, 0};
+        int[] workHours = {8, 6, 8, 9, 6, 0, 0, 6, 10, 8, 8, 6, 0, 0, 6, 10, 8, 8, 6, 0, 0};
         int count = workHours.length / 7;
         int[] hoursOfWeek = new int[count];
         int sum = 0;
+        int j = 0;
+        int a = 0;
 
-        for (int n = 0; n < count; n++) {
-            for (int i = 0; i < 7; i++) {
-                sum += workHours[i] - hoursOfWeek[n];
-            }
-            hoursOfWeek[n] = sum;
+        for (int i = 0; i < workHours.length; i++) {
         }
-        System.out.println("Работник работал за " + count + " недели " + Arrays.toString(hoursOfWeek) + " часов " +
-                "соответственно.");
     }
 }
+/*
+
+class NestedForLoop {
+    public static void main(String args[]) {
+        int i, j;
+        for (i = 1; i <= 3; i++) {
+            System.out.println(i + "iteration");
+           */
+/* for (j = 1; j <= 3; j++) {
+                System.out.println(j);
+            }*//*
+
+        }
+    }
+}*/
---
+++ src/main/java/ConditionalOperators/task7/TheBattle.java
@@ -0,0 +1,25 @@
+package ConditionalOperators.task7;
+
+public class TheBattle {
+    public static void main(String[] args) {
+        int sithPallatin = 100;
+        int jediObiVan = 120;
+        int sithDartMol = 100;
+        int sith = 80;
+        int jedi = 100;
+
+        if (sith > jedi) {
+            System.out.println("Джедай победит");
+        } else if (sith < jedi) {
+            System.out.println("Джедай победит");
+        } else if (sith == jedi) {
+            System.out.println("Ситх победит");
+        }
+    }
+    // Известно, что если ситх стоит выше джедая, ситх проиграет. Если джедай стоит выше
+    //ситха, джедай выигрывает. Если ситх и джедай на одной высоте, победа остаётся за
+    //ситхом.
+    //Но из этого правила есть исключения. Ситх Палпатин выигрывает всегда. Джедай Оби
+    //Ван выигрырывает у ситха Дарта Мола даже если они стоят на одной и той же высоте.
+
+}
