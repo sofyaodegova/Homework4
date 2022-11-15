5d6c609349c44c8d0c3c7557c7fb602500b63973
Sofya Odegova <sophiaodegova@mail.ru>
2022-11-15 17:55:10 +0400

    WorkWeek is done

--- src/main/java/Arrays/task12/WorkWeek.java
+++ src/main/java/Arrays/task12/WorkWeek.java
@@ -8,26 +8,10 @@ public class WorkWeek {
         int[] workHours = {8, 6, 8, 9, 6, 0, 0, 6, 10, 8, 8, 6, 0, 0, 6, 10, 8, 8, 6, 0, 0};
         int count = workHours.length / 7;
         int[] hoursOfWeek = new int[count];
-        int sum = 0;
-        int j = 0;
-        int a = 0;
 
         for (int i = 0; i < workHours.length; i++) {
+            hoursOfWeek[i / 7] += workHours[i];
         }
+        System.out.println(Arrays.toString(hoursOfWeek));
     }
 }
-/*
-
-class NestedForLoop {
-    public static void main(String args[]) {
-        int i, j;
-        for (i = 1; i <= 3; i++) {
-            System.out.println(i + "iteration");
-           */
-/* for (j = 1; j <= 3; j++) {
-                System.out.println(j);
-            }*//*
-
-        }
-    }
-}*/
