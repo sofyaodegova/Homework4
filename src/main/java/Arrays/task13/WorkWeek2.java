06907cd7c550b120deaa391a66d977e3f39394e4
Sofya Odegova <sophiaodegova@mail.ru>
2022-11-18 14:27:57 +0400

    WorkWeek2 is done

--- src/main/java/Arrays/task13/WorkWeek2.java
+++ src/main/java/Arrays/task13/WorkWeek2.java
@@ -5,8 +5,16 @@ import java.util.Arrays;
 public class WorkWeek2 {
     public static void main(String[] args) {
         int[] workHours = {8, 6, 8, 9, 6, 0, 0, 6, 10, 8, 8, 6, 0, 0, 6, 10, 8, 8};
-        int count = workHours.length / 7;
-        int[] hoursOfWeek = new int[count + 1];
+
+        int count;
+        if (workHours.length % 7 == 0) {
+            count = workHours.length / 7;
+        } else {
+            count = (workHours.length / 7) + 1;
+        }
+
+        int[] hoursOfWeek = new int[count];
+
 
         for (int i = 0; i < workHours.length; i++) {
             hoursOfWeek[i / 7] += workHours[i];
